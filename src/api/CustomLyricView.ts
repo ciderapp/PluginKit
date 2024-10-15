45389a10656ea0d15c47ed538143920683e46b21
booploops <49113086+booploops@users.noreply.github.com>
2024-10-14 20:41:26 -0700

    type

--- src/api/CustomLyricView.ts
+++ src/api/CustomLyricView.ts
@@ -1,12 +1,24 @@
 export interface CustomLyricViewProps {
+    /**
+     * Top offset of the lyrics view.  Used by Immersive to center the lyrics vertically.
+     */
     offsetY?: number;
+    /**
+     * Are sing lyrics disabled?
+     */
     noSingLyrics?: boolean;
-    simpleLyricStyle?: boolean;
+    /**
+     * Is this view in Immersive mode?
+     */
     isImmersive?: boolean;
-    zoomScale?: number;
+    /**
+     * When lyrics are not found, show a message indicating that no lyrics were found.
+     */
     showNoLyricsFoundText?: boolean;
+    /**
+     * Bound to the :style="" attribute of the lyrics view.
+     */
     elStyle?: { [key: string]: string };
-    type?: 'lyrics' | 'single-line' | 'basic';
 }
 
 export function addLyricView(props: CustomLyricViewProps) {
